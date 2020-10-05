7e5360fe0078c6f905ec7347cd7e53299688fed9
javser <javser.ru@gmail.com>
2020-10-05 21:56:41 +0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,11 +3,33 @@ import "./style.css";
 
 import Header from "./components/Header";
 
+function List ({items}) {
+  let ll = items.map((i,n)=><li key={n}>{i}</li>);
+  return <ul>{ll}</ul>
+}
+
+function Add ({onSubmit}) {
+  return (
+    <form action='' onSubmit={(e)=>{
+      onSubmit(e.target.newItem.value); 
+      e.preventDefault();
+      }
+    }>
+      <input type='text' name='newItem' />
+      <input type='submit' value='add' />
+    </form>
+  );
+}
+
 class App extends Component {
+  state = {items:[]};
+  handleSubmit = (val) => this.setState({items:this.state.items.concat([val])});
   render() {
     return (
       <div>
         <Header title={"todo-l-app"} />
+        <Add onSubmit={this.handleSubmit} />
+        <List items={this.state.items} />
       </div>
     );
   }
